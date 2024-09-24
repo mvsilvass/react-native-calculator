070b9bbdd6e1587442f642ef30c34ae8fb251d83
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-09-24 18:34:06 -0300

    Adiciona Button

---
+++ src/components/Buttons/Button.js
@@ -0,0 +1,33 @@
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+
+export default function Button({ textValue, onPress }) {
+  return (
+    <TouchableOpacity style={styles.button} onPress={onPress}>
+      <Text style={styles.buttonText}>{textValue}</Text>
+    </TouchableOpacity>
+  );
+}
+
+Button.propTypes = {
+    textValue: PropTypes.string.isRequired,
+    onPress: PropTypes.func.isRequired,
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#CDC1FF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: '#6C48C5',
+    paddingHorizontal: 30,
+    paddingVertical: 10,
+    flex: 1,
+  },
+  buttonText: {
+    textAlign: 'center',
+    fontSize: 30,
+  },
+});
