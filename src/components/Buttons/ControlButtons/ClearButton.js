de1d8e10b1aa44d886bb2a7c061ffed7c6f5493a
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-09-24 18:37:15 -0300

    Adiciona ClearButton

---
+++ src/components/Buttons/ControlButtons/ClearButton.js
@@ -0,0 +1,9 @@
+import { useContext } from 'react';
+import { CalculatorContext } from '../../../contexts/CalculatorContext';
+import Button from '../Button';
+
+export default function ClearButton() {
+  const { setInputExpression } = useContext(CalculatorContext);
+
+  return <Button textValue="AC" onPress={() => setInputExpression([])} />;
+}
