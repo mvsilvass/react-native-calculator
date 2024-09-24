b443a0cbfcd8c3fa64feeee7549cd40d0d3cfe14
Maria Vitoria <mvsilvasoaress@gmail.com>
2024-09-24 18:36:17 -0300

    Adiciona CalculateButton

---
+++ src/components/Buttons/ControlButtons/CalculateButton.js
@@ -0,0 +1,11 @@
+import { useContext } from 'react';
+import { CalculatorContext } from '../../../contexts/CalculatorContext';
+import Button from '../Button';
+
+export default function CalculateButton() {
+  const { setInputExpression, calculate } = useContext(CalculatorContext);
+
+  return (
+    <Button textValue="=" onPress={() => setInputExpression([calculate()])} />
+  );
+}
